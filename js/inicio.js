8fdba63f8919e71f8afa5dee5a9021e3565ce35d
Breno <bs646271@gmail.com>
2022-07-25 11:07:51 -0300

    Iniciando funcionalidades da página inicio.html

---
+++ js/inicio.js
@@ -0,0 +1,16 @@
+//Funcões dos botões (up/down) para saida.js
+// Saída trabalho
+// Saída pausa
+//Saída sessões
+
+//Funcções do botãp (continuar)
+
+
+/*/Área de testes
+
+let saidaTrabalho = document.querySelector('div.work-exit');
+saidaTrabalho.innerHTML = '25';
+saidaTrabalho.style.textAlign='center';
+saidaTrabalho.style.lineHeight='8rem';
+saidaTrabalho.style.fontSize='5rem';
+*/
