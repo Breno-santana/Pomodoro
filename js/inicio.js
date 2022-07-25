69834b01d99ce908addbc112a9f9070e7744be7b
Breno <bs646271@gmail.com>
2022-07-25 16:46:20 -0300

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -2,15 +2,5 @@
 // Saída trabalho
 // Saída pausa
 //Saída sessões
+//Funcções do botão (continuar)
 
-//Funcções do botãp (continuar)
-
-
-/*/Área de testes
-
-let saidaTrabalho = document.querySelector('div.work-exit');
-saidaTrabalho.innerHTML = '25';
-saidaTrabalho.style.textAlign='center';
-saidaTrabalho.style.lineHeight='8rem';
-saidaTrabalho.style.fontSize='5rem';
-*/
